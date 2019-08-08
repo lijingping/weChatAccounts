36abd0c46800d9040c6eddd8223c859f224c1909
dlijp <504136021@qq.com>
2019-08-08 10:35:01 +0800

    Revert "modify ‘web.xml ‘and add ‘index.jsp’ for welcome page"
    This reverts commit edf3ebcc5b9af8b8653d0d703034ec71074b0d76.

--- weChatServlet/src/weChatServlet/weChatAccounts.java
+++ weChatServlet/src/weChatServlet/weChatAccounts.java
@@ -1,10 +1,6 @@
 package weChatServlet;
 
 import java.io.IOException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,10 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/weChatAccounts")
 public class weChatAccounts extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	//自定义token, 用作生成签名,从而验证安全性
-    private final String TOKEN = "cherry";
-
+       
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -29,44 +22,12 @@ public class weChatAccounts extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("-----开始校验签名-----");
-
-        /**
-         * 接收微信服务器发送请求时传递过来的参数
-         */
-        String signature = req.getParameter("signature");
-        String timestamp = req.getParameter("timestamp");
-        String nonce = req.getParameter("nonce"); //随机数
-        String echostr = req.getParameter("echostr");//随机字符串
-        if (signature == null) {
-        	System.out.println("-----校验签名失败-----");
-        	return;
-        }
-        /**
-         * 将token、timestamp、nonce三个参数进行字典序排序
-         * 并拼接为一个字符串
-         */
-        String sortStr = sort(TOKEN,timestamp,nonce);
-        /**
-         * 字符串进行shal加密
-         */
-        String mySignature = shal(sortStr);
-        /**
-         * 校验微信服务器传递过来的签名 和  加密后的字符串是否一致, 若一致则签名通过
-         */
-        if(!"".equals(signature) && !"".equals(mySignature) && signature.equals(mySignature)){
-            System.out.println("-----签名校验通过-----");
-            resp.getWriter().write(echostr);
-        }else {
-            System.out.println("-----校验签名失败-----");
-        }
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
@@ -76,49 +37,5 @@ public class weChatAccounts extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
-	/**
-     * 参数排序
-     * @param token
-     * @param timestamp
-     * @param nonce
-     * @return
-     */
-    public String sort(String token, String timestamp, String nonce) {
-        String[] strArray = {token, timestamp, nonce};
-        Arrays.sort(strArray);
-        StringBuilder sb = new StringBuilder();
-        for (String str : strArray) {
-            sb.append(str);
-        }
-        return sb.toString();
-    }
 
-    /**
-     * 字符串进行shal加密
-     * @param str
-     * @return
-     */
-    public String shal(String str){
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-1");
-            digest.update(str.getBytes());
-            byte messageDigest[] = digest.digest();
-
-            StringBuffer hexString = new StringBuffer();
-            // 字节数组转换为 十六进制 数
-            for (int i = 0; i < messageDigest.length; i++) {
-                String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
-                if (shaHex.length() < 2) {
-                    hexString.append(0);
-                }
-                hexString.append(shaHex);
-            }
-            return hexString.toString();
-
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
 }
